19e91f889518ec1c1299a8c27e0c75bf212dc76b
alvin joseph valdez <alvin.consultant@platform-11.com>
2019-03-29 13:24:58 +0800

    added ignore

---
+++ P11PasswordField/Main/MainVC.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  P11PasswordField
+//
+//  Created by Novare Account on 29/03/2019.
+//  Copyright © 2019 Novare Account. All rights reserved.
+//
+
+import UIKit
+
+public class MainVC: UIViewController {
+    
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ P11PasswordField/Utilities/Extensions/UIViewExtension.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewExtension.swift
+//  P11PasswordField
+//
+//  Created by Novare Account on 29/03/2019.
+//  Copyright © 2019 Novare Account. All rights reserved.
+//
+
+import Foundation
