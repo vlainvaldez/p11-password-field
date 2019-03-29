0f0e5e19ed4f337f106c6b31fbbd54e7a57200b5
alvin joseph valdez <alvin.consultant@platform-11.com>
2019-03-29 13:26:32 +0800

    initial commit

--- P11PasswordField/Utilities/Extensions/UIViewExtension.swift
+++ P11PasswordField/Utilities/Extensions/UIViewExtension.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2019 Novare Account. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    
+    func subview(forAutoLayout subview: UIView) {
+        self.addSubview(subview)
+        subview.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func subviews(forAutoLayout subviews: UIView...) {
+        self.subviews(forAutoLayout: subviews)
+    }
+    
+    func subviews(forAutoLayout subviews: [UIView]) {
+        subviews.forEach(self.subview)
+    }
+    
+    func setRadius(radius: CGFloat? = nil) {
+        self.layer.cornerRadius = radius ?? self.frame.width / 2
+        self.layer.masksToBounds = true
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '11.0'
+
+target 'P11PasswordField' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for P11PasswordField
+  pod 'SnapKit'
+  target 'P11PasswordFieldTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'P11PasswordFieldUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
